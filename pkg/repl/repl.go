756a8fdf335b2ba4d1d930d175411058e3e6b4ee
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2019-02-25 02:32:10 +1000

    Add dumb completions

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -1,14 +1,39 @@
 package repl
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	prompt "github.com/c-bata/go-prompt"
 
 	"github.com/Nekroze/chell/pkg/parsing"
 )
 
+func getBinariesInPath() (binaries []prompt.Suggest) {
+	foundBinaries := map[string]bool{}
+
+	visitor := func(path string, f os.FileInfo, err error) error {
+		if f != nil && !f.Mode().IsDir() {
+			foundBinaries[f.Name()] = true
+		}
+		return err
+	}
+
+	for _, part := range strings.Split(os.Getenv("PATH"), ":") {
+		filepath.Walk(part, visitor)
+	}
+
+	for name, _ := range foundBinaries {
+		binaries = append(binaries, prompt.Suggest{name, ""})
+	}
+
+	return binaries
+}
+
 func completer(d prompt.Document) []prompt.Suggest {
 	return prompt.FilterHasPrefix(
-		[]prompt.Suggest{},
+		getBinariesInPath(),
 		d.GetWordBeforeCursor(),
 		true,
 	)
